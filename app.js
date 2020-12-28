92eff40acced38f50a2f82a8dc6c40dc653b4833
MarinaAreias <marinasareias@gmail.com>
2020-12-28 23:09:05 +0100

    audio and key animatiom
    - rewinde  audio, so it can be played multiple times without need to waite in order for key to play audio
    - selecting key to be animated

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 window.addEventListener('keydown', function(event){
     
  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-if (!audio) return; //stops the function from running
+ const key  = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
+ if (!audio) return; //stops the function from running
+
+audio.currentTime=0; //rewinde to start
 audio.play();
-console.log(audio);
+
+key.classList.add('playing');
+
 });
+
+const keys = document.querySelectorAll('.key');
